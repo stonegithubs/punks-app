4a84958b43b2c2065baed95ca20ae697b86c620c
Dustin Boersma <dustin.boersma@activision.com>
2022-01-23 18:58:07 -0800

    new home styling and content

--- src/components/PageHome.jsx
+++ src/components/PageHome.jsx
@@ -1,32 +1,55 @@
 import './PageHome.css';
 import React from 'react';
-import { useWeb3Context } from '../context/Web3Context';
 import AnchorAddress from './AnchorAddress';
 import SectionMint from './SectionMint';
-import GridImgSrc from '../images/grid.jpg';
+import PinataImgSrc from '../media/pinata.png';
+import PreviewVideoSrc from '../media/preview.mp4';
+import { BUTTPUNK_CONTRACT_MAP } from '../service/web3';
 
 function PageHome() {
-  const { web3State } = useWeb3Context();
   return (
     <div className="PageHome">
+      <div className="PageHome-section PageHome-section--intro">
+        <div className="PageHome-sectionMain">
+          <video className="PageHome-previewVideo" muted autoPlay loop>
+            <source src={PreviewVideoSrc} />
+          </video>
+          <p className="PageHome-blurb">
+            What&apos;s better than one punk? How about two of them smashed together and
+            then decorated with nine unique traits!
+          </p>
+        </div>
+      </div>
+      <div className="PageHome-section PageHome-section--details">
+        <div className="PageHome-sectionInner">
+          <div className="PageHome-sectionImg PageHome-sectionImg--pinata">
+            <img src={PinataImgSrc} alt="Pinata Logo" />
+          </div>
+          <div className="PageHome-sectionMain">
+            <h2 className="PageHome-headline">Give me the deets!</h2>
+            <p className="PageHome-blurb">
+              ButtPunks is a limited collection of 10,000 unique NFT butts living on
+              the Ethereum blockchain. Each butt is made unique by randomly generating a
+              combination of nine traits from a collection of over 50 of them.
+            </p>
+            <p className="PageHome-blurb">
+              Each butt and its metadata is stored on IPFS via Pinata
+              and you can assure there is no funny business since the directory CID
+              is stored right there in the smart contract:
+              {' '}
+              <AnchorAddress chainId={1} address={BUTTPUNK_CONTRACT_MAP[1]} />
+              . There&apos;s more information on that over on
+              {' '}
+              <a href="https://docs.pinata.cloud/nfts" target="_blank" rel="noreferrer">the Pinata website</a>
+              {' '}
+              but the tldr is that it&apos;s impossible for us to change the contents!
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="PageHome-section PageHome-section--butts">
         <h2 className="PageHome-headline">Mint Some Butts!</h2>
         <SectionMint />
-      </div>
-      <div className="PageHome-section">
-        <img className="PageHome-mainImg" src={GridImgSrc} alt="A sample grid of ButtPunks" />
-        <p className="PageHome-blurb">
-          ButtPunks is a limited collection of 10,000 unique NFT butts living on
-          the Ethereum blockchain. Each butt was carefully crafted by smashing
-          two punks together and then assigning it a unique combination of nine traits.
-        </p>
-        <p className="PageHome-blurb">
-          Blurb about NFT Integrety. IPFS/Pinata provenance record.
-          No fishy business.
-          Verified smart contract address:
-          {' '}
-          <AnchorAddress chainId={web3State.chainId} address={web3State.buttpunkContractAddress} />
-        </p>
       </div>
     </div>
   );
