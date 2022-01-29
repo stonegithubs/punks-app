0a28ddd7390a225d8a522088b4c6794e08dd9df5
Dustin Boersma <1715242+pBun@users.noreply.github.com>
2022-01-28 23:55:08 -0800

    Clean up

--- contracts/ButtPunks.sol
+++ contracts/ButtPunks.sol
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
