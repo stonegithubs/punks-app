24e9d1776424e8d2e755655d3dd69d52807a2a40
Dustin Boersma <1715242+pBun@users.noreply.github.com>
2022-01-29 01:26:26 -0800

    Optimization

--- contracts/ButtPunks.sol
+++ contracts/ButtPunks.sol
@@ -3,24 +3,21 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 contract ButtPunks is ERC721, Ownable, PaymentSplitter {
     using SafeMath for uint256;
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-
-    uint256 public constant MAX_SUPPLY = 10000;
-    uint256 public constant MAX_PURCHASE = 20;
+    
     uint256 public constant TOKEN_PRICE = 0.0001 ether;
     address[] private constant ADDRESS_LIST = [
         0xD1aDe89F8826d122F0a3Ab953Bc293E144042539,
         0x4a4F584cA801192D459aFDF93BE3aE2C627FF8a2
     ];
-    uint256[] private constant SHARE_LIST = [50, 50];
+    uint8[] private constant SHARE_LIST = [50, 50];
+    uint16 public constant MAX_SUPPLY = 10000;
+    uint16 public constant MAX_PURCHASE = 20;
 
     bool public saleIsActive = false;
 
@@ -43,27 +40,28 @@ contract ButtPunks is ERC721, Ownable, PaymentSplitter {
         return saleIsActive;
     }
 
-    function mintToken(uint256 numberOfTokens) public payable {
+    function mintToken(uint8 numberOfTokens) public payable {
+        uint16 curTotal = totalSupply();
+        uint16 newTotal = curTotal.add(numberOfTokens);
+        
         require(saleIsActive, "Sale must be active to mint a token");
         require(
             numberOfTokens <= MAX_PURCHASE,
             "Each wallet can only mint 20 tokens at a time"
         );
         require(
-            _tokenIds.current().add(numberOfTokens) <= MAX_SUPPLY,
+            newTotal <= MAX_SUPPLY,
             "Purchase would exceed max supply of tokens"
         );
         require(
             TOKEN_PRICE.mul(numberOfTokens) <= msg.value,
-            "Ether value sent is not correct"
+            "Ether value sent is lower than expected"
         );
 
-        for (uint256 i = 0; i < numberOfTokens; i++) {
-            _tokenIds.increment();
-            uint256 newItemId = _tokenIds.current();
-            if (newItemId < MAX_SUPPLY) {
-                _safeMint(msg.sender, newItemId);
-            }
+        uint16 newTokenId = newTotal;
+        while (newTokenId < newTotal) {
+            _safeMint(msg.sender, newTokenId);
+            newTokenId = newTokenId.add(1);
         }
     }
 }
